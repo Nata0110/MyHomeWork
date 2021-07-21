e99a1897f97a0b02f5da401288732d91c785676c
Nata <martynovychn@gmail.com>
2021-07-21 20:24:43 +0300

    first task

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <string>
 
-using namespace std;
+void print()
+{
+    std::cout <<"Hello! Who are you? "<<std::endl;
+}
+
+void answer()
+{
+    std::string question;
+    int age;
+    print ();
+    std::cin >> question;
+    std::cout <<"How old are you?"<< std::endl;
+    std::cin >> age;
+    std::cout << "Hello " << question <<", " << age << " is the best age!!!"<< std::endl;
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    answer();
 }
